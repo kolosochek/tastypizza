8b652b11ed0330aa07e9e5976b74177b44f25f92
D <kolosochek@gmail.com>
2022-07-07 21:19:48 +0300

    changed way of css font import, again, and again

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import store from '../store.js'
 // css
 import '../styles/globals.css'
-
+import '../styles/fonts.css'
 
 function MyApp({ Component, pageProps }) {
   return <Provider store={store}>
